4776b8f3ff0ff4302b0deaeece07cc159c963709
arielf2 <yamiyami260@gmail.com>
2020-01-06 18:26:33 +0200

    add client project and server project to same solution add some client files

---
+++ group01_ex4_client/Header.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define MAX_USERNAME_LENGTH 21  // 20 + \0
+#define MAX_IP_LENGTH 16 // 255.255.255.255, 15 + 1
+#define MAX_PORT_LENGTH 5 // 8888, + 1
+
+/*	Description: Initial parser for the command line arguments
+	Parameters:  ip_address: string representing the ip address, to be updated in the function
+				 port: string representing the port, to be updated in the function
+				 username: string representing the ip username, to be updated in the function
+				 arguments: array of strings: the actual command line arguments
+	Returns:	 error code: 1 upon succces, 0 upon failure */
+
+int ParseCommand(char* ip_address, char* port, char* username, char* arguments[]);
---
+++ group01_ex4_client/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Header.h"
+#include "Client.h"
+
+int main(int argc, char* argv[]) {
+
+	char username[MAX_USERNAME_LENGTH], port[MAX_PORT_LENGTH], ip_address[MAX_IP_LENGTH];
+	if (ParseCommand(ip_address, port, username, argv)) {
+		return 1;
+	}
+
+	MainClient(ip_address, port, username);
+
+}
+
+
+int ParseCommand(char* ip_address, char* port, char* username, char* arguments[]) {
+
+	int err = 0;
+
+	if (strcpy_s(ip_address, MAX_IP_LENGTH, arguments[1])) {
+		printf("Error while receiving ip address from command line\n");
+		err = 1;
+	}
+	if (strcpy_s(port, MAX_PORT_LENGTH, arguments[2])) {
+		printf("Error while receiving port from command line\n");
+		err = 1;
+	}
+	if (strcpy_s(username, MAX_USERNAME_LENGTH, arguments[3])) {
+		printf("Error while receiving username from command line\n");
+		err = 1;
+	}
+
+}

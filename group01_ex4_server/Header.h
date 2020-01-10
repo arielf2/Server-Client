3154493aee4e66575d2390a7b80ec38997df6cfc
rotem <rotemhec@gmail.com>
2020-01-10 11:02:15 +0200

    added all files

---
+++ group01_ex4_server/Header.h
@@ -0,0 +1,26 @@
+#pragma once
+/*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
+/*
+ This file was written for instruction purposes for the
+ course "Introduction to Systems Programming" at Tel-Aviv
+ University, School of Electrical Engineering, Winter 2011,
+ by Amnon Drory.
+*/
+/*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
+
+#include <stdio.h>
+#include <string.h>
+#include <winsock2.h>
+#include "SendRecv.h"
+
+/*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
+
+static int FindFirstUnusedThreadSlot();
+static void CleanupWorkerThreads();
+static DWORD ServiceThread(SOCKET *t_socket);
+int write_in_leader_board(char user_name[], int win);
+int parse_command(char *command, char* message_type, char* parameters) {
+void write_move_to_file(char *move);
+int send_leader_board(SOCKET *t_socket);
+/*oOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoO*/
+
